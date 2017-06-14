79abac997b201e408c4134b0745efa97ed923dfa
Bineesh M.G <bineeshmg@hotmail.com>
2017-06-14 12:47:48 +0530

    minor change

--- sw.js
+++ sw.js
@@ -24,6 +24,7 @@
 var dataCacheName = 'pwaTestData-v1';
 var cacheName = 'pwaTestfinal-1';
 var filesToCache = [    
+    'https://astoriasoft.github.io/amp/',
     'https://astoriasoft.github.io/amp/index.html',
     'https://astoriasoft.github.io/amp/scripts/main.js',
     'https://astoriasoft.github.io/amp/styles/index.css',    
