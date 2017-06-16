73b009fcdd42e6fdef3992708cc6dbabf35201d8
Bineesh M.G <bineeshmg@hotmail.com>
2017-06-16 10:30:03 +0530

    a

--- sw.js
+++ sw.js
@@ -1,33 +1,10 @@
-/*
-*
-*  Push Notifications codelab
-*  Copyright 2015 Google Inc. All rights reserved.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License
-*
-*/
-
-/* eslint-env browser, serviceworker, es6 */
-
 'use strict';
 
 var dataCacheName = 'pwaTestData-v1';
-var cacheName = 'pwaTestfinal-1';
-var filesToCache = [    
-    '/amp/',
-    '/amp/index.html',
-    '/amp/scripts/main.js',
-    '/amp/manifest.json',
+var CACHE_NAME = 'pwaTestfinal-1';
+var resourcesToCache = [    
+    '/amp/',    
+    '/amp/scripts/main.js',    
     '/amp/styles/index.css',    
     '/amp/images/icon.png',
     '/amp/images/badge.png',
@@ -43,49 +20,41 @@ var filesToCache = [
 self.addEventListener('install', function (e) {
     console.log('[ServiceWorker] Install');
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
+        caches.open(CACHE_NAME).then(function (cache) {
             console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
+            return cache.addAll(resourcesToCache);
         })
     );
 });
 
-self.addEventListener('activate', function (e) {
-    console.log('[ServiceWorker] Activate');
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if (key !== cacheName && key !== dataCacheName) {
-                    console.log('[ServiceWorker] Removing old cache', key);
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
+//self.addEventListener('activate', function (e) {
+//    console.log('[ServiceWorker] Activate');
+//    e.waitUntil(
+//        caches.keys().then(function (keyList) {
+//            return Promise.all(keyList.map(function (key) {
+//                if (key !== cacheName && key !== dataCacheName) {
+//                    console.log('[ServiceWorker] Removing old cache', key);
+//                    return caches.delete(key);
+//                }
+//            }));
+//        })
+//    );
 
     self.addEventListener('fetch', function (event) {
         event.respondWith(
+            // try to find corresponding response in the cache
             caches.match(event.request)
                 .then(function (response) {
-                    return response || fetch(event.request);
+                    if (response) {
+                        // cache hit: return cached result
+                        return response;
+                    }
+
+                    // not found: fetch resource from the server
+                    return fetch(event.request);
                 })
         );
     });
-
-    
-
-    /*
-     * Fixes a corner case in which the app wasn't returning the latest data.
-     * You can reproduce the corner case by commenting out the line below and
-     * then doing the following steps: 1) load app for first time so that the
-     * initial New York City data is shown 2) press the refresh button on the
-     * app 3) go offline 4) reload the app. You expect to see the newer NYC
-     * data, but you actually see the initial data. This happens because the
-     * service worker is not yet activated. The code below essentially lets
-     * you activate the service worker faster.
-     */
-    return self.clients.claim();
-});
 
 self.addEventListener('push', function (event) {
     console.log('[Service Worker] Push Received.');
